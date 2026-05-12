7bbfaa16591a3eaac27c1db5c974ee6ba1b411ec
alisanaik <alisanaik111@gmail.com>
2026-05-13 00:32:04 +0530

    some changes

--- matrix_add.cu
+++ matrix_add.cu
@@ -20,47 +20,47 @@ __global__ void matrix_addition(int *A, int *B, int *C, int size) {
 int main() {
     const int matrix_size = N * N * sizeof(int);
     
-    // allocate memory on host
+    //allocate memory on host
     int *h_A = new int[N * N];
     int *h_B = new int[N * N];
     int *h_C = new int[N * N];
     
-    // initialize matrices
+    //initialize matrices
     for (int i = 0; i < N * N; ++i) {
             h_A[i] = 1;
             h_B[i] = 2;
             h_C[i] = 0;
     }
     
-    // allocate memory on device
+    //allocate memory on device
     int *d_A, *d_B, *d_C;
     cudaMalloc((void**)&d_A, matrix_size);
     cudaMalloc((void**)&d_B, matrix_size);
     cudaMalloc((void**)&d_C, matrix_size);
     
-    // copy data from host to device
+    //copy data from host to device
     cudaMemcpy(d_A, h_A, matrix_size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, matrix_size, cudaMemcpyHostToDevice);
     
-    // before calling the kernel, define block and grid sizes
+    //before calling the kernel, define block and grid sizes
     dim3 blockDim(32, 16); // number of threads per block (32*16 = 512 threads per block)
     dim3 gridDim((int)ceil((float)N / blockDim.x), (int)ceil((float)N / blockDim.y)); //represents the blocksize. 10 / 32, 10 / 16 = (1,1) 
     
-    // launch kernel
+    //launch kernel
     matrix_addition<<<gridDim, blockDim>>>(d_A, d_B, d_C, N);
 
      cudaDeviceSynchronize(); // wait for the kernel to finish before accessing the result
     
-     // copy result back to host
+    //copy result back to host
     cudaMemcpy(h_C, d_C, matrix_size, cudaMemcpyDeviceToHost);
     
-    // print result
+    //print result
     std::cout << "Result of matrix addition:" << std::endl;
     for (int i = 0; i < N*N; ++i) {
         std::cout << h_C[i] << ", ";
     }
     
-    // free memory
+    //free memory
     delete[] h_A;
     delete[] h_B;
     delete[] h_C;
